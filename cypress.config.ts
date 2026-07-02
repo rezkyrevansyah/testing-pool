315ab101b94b046ec1bd7ff5b27f77f8e7cfabab
rezkyrevansyah <rezky.revansyah@gmail.com>
2026-07-02 14:35:25 +0700

    Update Node.js version to 22 in workflows and configure reporter options in Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   e2e: {
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.ts',
+    reporter: 'json',
+    reporterOptions: {
+      output: 'cypress/results/results.json',
+    },
   },
 })
